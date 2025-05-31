aebe3f19f6d419dd176db1997a2cc685d80d56e9
zunairamarryum <zunairamarryum111@gmail.com>
2025-05-31 15:13:42 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Fragment, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <Fragment>
+   <h1>hello</h1>
+   <h1>heloo</h1>
+
+  </Fragment>
+
+  
+  )
+}
+
+export default App
